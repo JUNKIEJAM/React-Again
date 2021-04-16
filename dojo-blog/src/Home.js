131c86a226343d6ef7bd061b77b6a104fb25d037
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-04-17 00:36:23 +0530

    Fifth Lesson

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,23 +1,26 @@
 import { useState } from 'react';
 const Home = () => {
-    //let name = 'mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-    const handleClick = () => {
-        setName('luigi');
-    }
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( <
-        div className = "home" >
+        div className = "home" > {
+            blogs.map((blog) => ( <
+                div className = "blog-preview"
+                key = { blog.id } >
+                <
+                h2 > { blog.title } < /h2> <
+                p > Written by { blog.author } < /p>
 
-        <
-        h2 > Homepage < /h2>  <
-        p > { name }
-        is { age }
-        years old < /p> <
-        p > { name } < /p> <
-        button onClick = { handleClick } > Click me < /button>   < /
-        div >
+                <
+                /div>
+            ))
+        } <
+        /div>
     );
 }
 
