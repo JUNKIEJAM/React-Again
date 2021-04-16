59fe3b73ccc0735eac546cc58855c6a19d4ba01a
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-04-17 00:08:18 +0530

    Fourth Lesson

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,8 +1,22 @@
+import { useState } from 'react';
 const Home = () => {
+    //let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+    const handleClick = () => {
+        setName('luigi');
+    }
+
     return ( <
         div className = "home" >
+
         <
-        h2 > Homepage < /h2> < /
+        h2 > Homepage < /h2>  <
+        p > { name }
+        is { age }
+        years old < /p> <
+        p > { name } < /p> <
+        button onClick = { handleClick } > Click me < /button>   < /
         div >
     );
 }
