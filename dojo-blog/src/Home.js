e5f3a531b8136dfb4eb20092e3ee6a3f439593e4
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-04-21 20:45:42 +0530

    Custom Hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,30 +1,18 @@
-import { useState, useEffect } from "react";
-import BlogList from "./BlogList";
-
+import { useEffect, useState } from "react";
+import BlogList from './BlogList';
+import useFetch from './useFetch';
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false);
-                });
-        }, 1000);
-    }, []);
+        const { data: blogs, isPending, error } = useFetch('http://ocalhost:8000/blogs');
 
-    return ( <
-        div className = "home" > {
-            isPending && < div > Loading.... < /div>} {
-            blogs && < BlogList blogs = { blogs }
-            title = "All Blogs!" / >
-        } <
-        /div >
-    );
-}
 
-export default Home;
+        return ( <
+            div className = "home" > {
+                error && < div > { error } < /div> } {
+                isPending && < div > Loading... < /div> } {
+                blogs && < BlogList blogs = { blogs }
+                /> } < /
+                div >
+            );
+        }
+
+        export default Home;
