f975ffcb54ab72dd48b0c13cf13b05c0c1055444
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2021-04-22 20:00:41 +0530

    Input Form

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,26 +1,22 @@
-const BlogList = ({ blogs, title }) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
-    // console.log(blogs);
+import { Link } from 'react-router-dom';
+
+const BlogList = ({ blogs }) => {
     return ( <
-            div className = "blog-list" >
-            <
-            h2 > { title } < /h2> {
+        div className = "blog-list" > {
             blogs.map(blog => ( <
                 div className = "blog-preview"
                 key = { blog.id } >
                 <
-                h2 > { blog.title } < /h2> <
-                p > Written by { blog.author } < /p> 
-
-
-
+                Link to = { `/blogs/${blog.id}` } >
                 <
-                /div >
+                h2 > { blog.title } < /h2> <
+                p > Written by { blog.author } < /p> < /
+                Link > <
+                /div>
             ))
         } <
         /div>
-);
+    );
 }
 
 export default BlogList;
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
-import BlogList from './BlogList';
-import useFetch from './useFetch';
-const Home = () => {
-        const { data: blogs, isPending, error } = useFetch('http://ocalhost:8000/blogs');
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
+const Home = () => {
+        const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
         return ( <
             div className = "home" > {
